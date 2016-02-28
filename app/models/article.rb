f5512192d5f24bc0e3bb06d981bead731c87e2a0
chipset <tu.truonganh508@gmail.com>
2016-02-28 16:45:25 +0700

    test

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,6 @@
 class Article < ActiveRecord::Base
   def self.search(query)
     # where(:title, query) -> This would return an exact match of the query
-    where("title like ?", "#{query}")
+    where("title like ?", "%#{query}%")
   end
 end
