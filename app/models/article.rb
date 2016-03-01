18f039a8862fa7b323a28b25bed66bc4879bca75
chipset <tu.truonganh508@gmail.com>
2016-03-01 17:51:48 +0700

    validation

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,6 @@
 class Article < ActiveRecord::Base
+  validates :title, presence: true
+  validates :body, presence: true
   def self.search(query)
     # where(:title, query) -> This would return an exact match of the query
     where("title like ?", "%#{query}%")
